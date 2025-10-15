7569fa7f1c5b9f97fccf72446942ab92606de339
Akhil Sharma <akhilsharma0321k@gmail.com>
2025-10-15 15:59:58 +0530

    Final commt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // src/App.js
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Home from './pages/Home';
